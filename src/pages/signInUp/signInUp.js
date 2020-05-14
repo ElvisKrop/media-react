8b57505cbf5b491a68cb6681d379086406adc9cc
Nikita <needsthxxx@gmail.com>
2020-05-14 16:30:09 +0300

    Test separation of the registration and login page

--- src/pages/signInUp/signInUp.js
+++ src/pages/signInUp/signInUp.js
@@ -10,31 +10,13 @@ import ErrorList from "../../components/errorList/errorList";
 const SignInUp = ({
   type,
   isToken,
-  mrService,
   errors,
-  user,
   loading,
   userLoaded,
   userLoading,
   clearErrors,
   userLoadFail
 }) => {
-  const [username, setUsername] = useState("");
-  //TODO delete this in production version
-  const [email, setEmail] = useState("loginloginlogin@mail.com");
-  const [password, setPassword] = useState("loginloginlogin_pass_12345");
-
-  const makeBlank = useCallback(() => {
-    setUsername("");
-    setEmail("");
-    setPassword("");
-  }, []);
-
-  useEffect(() => {
-    // TODO turn on in production
-    //   makeBlank();
-  }, [type, makeBlank]);
-
   useEffect(() => {
     new Promise((resolve, reject) => {
       if (!Object.keys(errors).length) reject();
@@ -65,6 +47,66 @@ const SignInUp = ({
     }
   })(type);
 
+  // TODO errorlines(another component) for handling service errors
+  if (isToken) return <Redirect to="/" />;
+  if (loading && !Object.keys(errors).length)
+    return (
+      <div className="container d-flex wrapper">
+        <Spinner />
+      </div>
+    );
+  return (
+    <>
+      <div className="container d-flex wrapper">
+        <div className="col-lg-6 col-sm-10 col-11 mt-5 text-center">
+          <h1>Sign {settings.head}</h1>
+          <Link to={`/${settings.linkTo}`}>
+            {settings.linkWord} an account?
+          </Link>
+          <Test {...{ settings, userLoadFail, userLoaded, userLoading }} />
+        </div>
+      </div>
+      {Object.keys(errors).length ? <ErrorList errors={errors} /> : null}
+    </>
+  );
+};
+
+const mapStateToProps = ({ errors, loading }) => ({
+  errors,
+  loading
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  userLoaded: (user) => dispatch(Actions.userLoaded(user)),
+  userLoading: () => dispatch(Actions.userLoading()),
+  userLoadFail: (errors) => dispatch(Actions.userLoadFail(errors)),
+  clearErrors: () => dispatch(Actions.clearErrors())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withToken(SignInUp));
+
+const Test = withService()(_Test);
+
+function _Test({ settings, mrService, userLoadFail, userLoaded, userLoading }) {
+  const [username, setUsername] = useState("");
+  //TODO In production mode: delete default values
+  const [email, setEmail] = useState("loginloginlogin@mail.com");
+  const [password, setPassword] = useState("loginloginlogin_pass_12345");
+
+  const makeBlank = useCallback(() => {
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  }, []);
+
+  useEffect(() => {
+    // TODO In production mode: turn on this
+    //   makeBlank();
+  }, [makeBlank]);
+
   const sendFormFields = (event) => {
     event.preventDefault();
     userLoading();
@@ -87,79 +129,42 @@ const SignInUp = ({
       .catch(({ errors }) => userLoadFail(errors));
   };
 
-  // TODO errorlines(another component) for handling service errors
-  if (isToken) return <Redirect to="/" />;
-  if (loading && !Object.keys(errors).length)
-    return (
-      <div className="container d-flex wrapper">
-        <Spinner />
-      </div>
-    );
   return (
-    <>
-      <div className="container d-flex wrapper">
-        <div className="col-lg-6 col-sm-10 col-11 mt-5 text-center">
-          <h1>Sign {settings.head}</h1>
-          <Link to={`/${settings.linkTo}`}>
-            {settings.linkWord} an account?
-          </Link>
-          <form onSubmit={(e) => sendFormFields(e)}>
-            <fieldset>
-              {settings.isUsername && (
-                <div className="form-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </div>
-              )}
-              <div className="form-group">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Enter email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Sign {settings.head}
-              </button>
-            </fieldset>
-          </form>
+    <form onSubmit={(e) => sendFormFields(e)}>
+      <fieldset>
+        {settings.isUsername && (
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+        )}
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-      </div>
-      {Object.keys(errors).length ? <ErrorList errors={errors} /> : null}
-    </>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Sign {settings.head}
+        </button>
+      </fieldset>
+    </form>
   );
-};
-
-const mapStateToProps = ({ errors, user, loading }) => ({
-  errors,
-  user,
-  loading
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  userLoaded: (user) => dispatch(Actions.userLoaded(user)),
-  userLoading: () => dispatch(Actions.userLoading()),
-  userLoadFail: (errors) => dispatch(Actions.userLoadFail(errors)),
-  clearErrors: () => dispatch(Actions.clearErrors())
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(withService()(withToken(SignInUp)));
+}
