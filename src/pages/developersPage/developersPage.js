dba0fd58d87e77cac525b2c9432555cc52408c06
Nikita <needsthxxx@gmail.com>
2020-05-30 18:30:42 +0300

    back up, before replacing useState

--- src/pages/developersPage/developersPage.js
+++ src/pages/developersPage/developersPage.js
@@ -7,7 +7,7 @@ const developerPetr = {
     "https://avatars0.githubusercontent.com/u/57825512?s=400&u=f518af1421e520fff1660bbfe4e180dbbdd5ccef&v=4",
   profession: "Front-end developer",
   linkedin: "https://www.linkedin.com/in/petr-mikhailau-8020981a8/",
-  telegram: "https://t-do.ru/musakius",
+  telegram: "https://t.me/musakius",
   github: "https://github.com/musakius"
 };
 const developerNikita = {
@@ -16,7 +16,7 @@ const developerNikita = {
     "https://media-exp1.licdn.com/dms/image/C4D03AQHeDjjpe_uFAw/profile-displayphoto-shrink_800_800/0?e=1596067200&v=beta&t=GNjX4unptp-vlJpwclg8e78EYPfhbPgUus_SDftbK5M",
   profession: "Front-end developer",
   linkedin: "https://www.linkedin.com/in/nikita-zasimuk/",
-  telegram: "https://t-do.ru/ElvisKrop",
+  telegram: "https://t.me/ElvisKrop",
   github: "https://github.com/ElvisKrop"
 };
 
@@ -32,8 +32,7 @@ function DevelopersPage() {
         href="https://github.com/ElvisKrop/media-react"
         title="github"
         target="_blank"
-        rel="noopener noreferrer"
-      >
+        rel="noopener noreferrer">
         <i className="fab fa-github pr-1" />
         Open code on github
       </a>
@@ -46,7 +45,7 @@ function Card({ developer }) {
   return (
     <div className="card mb-3 p-0 card-max-height">
       <div className="card-size-img">
-        <img className="img-developer" src={img} alt="imgPetr" />
+        <img className="img-developer" src={img} alt="imgDev" />
       </div>
       <h4 className="list-group-item border-right-0 border-left-0">{name}</h4>
       <h6 className="list-group-item border-right-0 border-left-0">
@@ -57,24 +56,21 @@ function Card({ developer }) {
           href={linkedin}
           title="linkedin"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <i className="fab fa-linkedin-in align-items-center" />
         </a>
         <a
           href={telegram}
           title="telegram"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <i className="fab fa-telegram-plane" />
         </a>
         <a
           href={github}
           title="github"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <i className="fab fa-github" />
         </a>
       </div>
